e1385cae6c636f30e1adf99643bf2c0c99daa419
Litchi Pi <litchi.pi@proton.me>
2024-07-11 12:58:06 +0200

    Upgrade flake inputs
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOs config builder";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     localnixpkgs.url = "path:/home/john/work/nix/nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
@@ -13,7 +13,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
