58af1566c8a3dad0b099932e70ca7a7776e17f27
Litchi Pi <litchi.pi@proton.me>
2024-07-19 12:12:52 +0200

    Setup testing environment for firefly-iii
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- module.nix
+++ module.nix
@@ -1,2 +1,11 @@
 {config, pkgs, lib, ...}: {
+  services.firefly-iii = {
+    enable = true;
+    settings.APP_KEY_FILE = "/appkeyfile";
+  };
+
+  system.activationScripts.setup_keyfile = ''
+    echo "mysecretpassword" > /appkeyfile
+    chown firefly-iii:firefly-iii /appkeyfile
+  '';
 }
